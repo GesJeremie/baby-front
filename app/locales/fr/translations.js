24ed42107b504f7a2190dbf4cbf1b2a0075ada48
Jeremie ges <ges.jeremie@gmail.com>
2016-05-23 00:09:24 +0300

    translate validation errors in french

--- app/locales/fr/translations.js
+++ app/locales/fr/translations.js
@@ -1,34 +1,36 @@
 export default {
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
   errors: {
-    inclusion: "is not included in the list",
-    exclusion: "is reserved",
-    invalid: "is invalid",
-    confirmation: "doesn't match {{attribute}}",
-    accepted: "must be accepted",
-    empty: "ne peut être vide",
-    blank: "ne peux être vide",
-    present: "est requis",
-    tooLong: "is too long (maximum is {{count}} characters)",
-    tooShort: "is too short (minimum is {{count}} characters)",
-    wrongLength: "is the wrong length (should be {{count}} characters)",
-    notANumber: "is not a number",
-    notAnInteger: "must be an integer",
-    greaterThan: "must be greater than {{count}}",
-    greaterThanOrEqualTo: "must be greater than or equal to {{count}}",
-    equalTo: "must be equal to {{count}}",
-    lessThan: "must be less than {{count}}",
-    lessThanOrEqualTo: "must be less than or equal to {{count}}",
-    otherThan: "must be other than {{count}}",
-    odd: "must be odd",
-    even: "must be even"
+    description: "Ce champ",
+    inclusion: "{{description}} n'est pas inclus dans la liste",
+    exclusion: "{{description}} est reservé",
+    invalid: "{{description}} est invalide",
+    confirmation: "{{description}} n'est pas égale à {{on}}",
+    accepted: "{{description}} doit être accepté",
+    empty: "{{description}} est requis",
+    blank: "{{description}} est requis",
+    present: "{{description}} est requis",
+    collection: "{{description}} doit être une collection",
+    singular: "{{description}} ne peux pas être une collection",
+    tooLong: "{{description}} est trop long (maximum est de {{max}} caractères)",
+    tooShort: "{{description}} est trop court (minimum est de {{min}} caractères)",
+    before: "{{description}} doit être avant {{before}}",
+    after: "{{description}} doit être après {{after}}",
+    wrongDateFormat: "{{description}} doit être dans le format de {{format}}",
+    wrongLength: "{{description}} doit être de {{is}} caractères",
+    notANumber: "{{description}} doit être un nombre",
+    notAnInteger: "{{description}} doit être un entier",
+    greaterThan: "{{description}} doit être plus grand à {{gt}}",
+    greaterThanOrEqualTo: "{{description}} doit être plus grand ou égal à {{gte}}",
+    equalTo: "{{description}} doit être égal à {{is}}",
+    lessThan: "{{description}} doit être inférieur à {{lt}}",
+    lessThanOrEqualTo: "{{description}} doit être inférieur ou égal à {{lte}}",
+    otherThan: "{{description}} doit être une autre valeur  que {{value}}",
+    odd: "{{description}} doit être pair",
+    even: "{{description}} doit être impair",
+    positive: "{{description}} doit être positif",
+    date: "{{description}} doit être une date valide",
+    email: "{{description}} doit être une adresse email valide",
+    phone: "{{description}} doit être un numéro de téléphone valide",
+    url: "{{description}} doit être une url valide"
   }
 };
